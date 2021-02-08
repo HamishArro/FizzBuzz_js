a45d82f503698d93565ac4751c9ab66f5e215e65
Hamish Arro <Hamisharro@icloud.com>
2021-02-08 16:36:52 +0000

    finish FizzBuzz

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,25 @@
+describe("FizzBuzz", function() {
+  var subject;
+
+  beforeEach(function() {
+    console.log('creating subject');
+    subject = new FizzBuzz;
+  });
+
+  it("returns Fizz when 3 is entered", function() {
+    expect(subject.input(3)).toEqual('Fizz');
+  });
+
+  it("returns Buzz when 5 is entered", function() {
+    expect(subject.input(5)).toEqual('Buzz');
+  });
+
+  it("returns FizzBuzz when 15 is entered", function() {
+    expect(subject.input(15)).toEqual('FizzBuzz');
+  });
+
+  it("returns 2 when 2 is entered", function() {
+    expect(subject.input(2)).toEqual(2);
+  });
+
+});
---
+++ src/fizzBuzz.js
@@ -0,0 +1,10 @@
+class FizzBuzz {
+
+  input(number) {
+    if (number % 3 === 0 && number % 5 === 0) { return 'FizzBuzz' }
+    else if (number % 5 === 0) { return 'Buzz' }
+    else if (number % 3 === 0) { return 'Fizz' }
+    else { return number };
+  }
+
+}
